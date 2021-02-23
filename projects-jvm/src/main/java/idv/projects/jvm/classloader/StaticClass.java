9834bdeec396905ba9f7b3b7594bb858b3d29694
from scratch <shaoqing_wu@foxmail.com>
2021-02-23 23:37:00 +0800

    add static fields

---
+++ projects-jvm/src/main/java/idv/projects/jvm/classloader/StaticClass.java
@@ -0,0 +1,17 @@
+package idv.projects.jvm.classloader;
+
+/**
+ * @author shaoq 2021/2/23 22:19
+ */
+public class StaticClass {
+
+    public static void Method() {
+        String a = new String("abccccc").intern();
+        int b = 2 * (2 + 6);
+    }
+
+    public static void main(String[] args) {
+        StaticClass.Method();
+    }
+
+}
